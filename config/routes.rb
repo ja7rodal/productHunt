a0f3303b6ea17f18ffcd29ab82361cff63275117
Jasson Rdz <ja7rodal@gmail.com>
2016-10-09 20:07:37 -0500

    create show one product

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   get '/products', to: 'products#index'
-  get '/products/new', to: 'products#new', as: 'new_product' 
+  get '/products/new', to: 'products#new', as: 'new_product'
   post '/products', to: 'products#create'
+  get '/products/:id', to: 'products#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
