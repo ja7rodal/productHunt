7fed030ec964a7345360c55f61a2960fd38fbeb4
Jasson Rdz <ja7rodal@gmail.com>
2016-10-10 23:13:48 -0500

    add function implementar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  root 'products#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
 
   resources :users, only: [:new, :create]
   resources :products
-  root 'products#index'
+
   # root 'products#index' only: [:show]
   # root 'products#index' except: [:show,:update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
