12ca58bb94f93983283baa16fc892cf721cfbaa2
Jasson Rdz <ja7rodal@gmail.com>
2016-10-14 21:37:09 -0500

    change language es

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module ProductHunt
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :es
+    config.i18n.enforce_available_locales = true
+    #config.active_record.raise_in_transactional_Callbacks = true
   end
 end
